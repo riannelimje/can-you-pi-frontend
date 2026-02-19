e2097b17640824a28b741689df4f38963f7a2744
Rianne Lim <rianne.limje@gmail.com>
2026-02-19 14:09:33 +0800

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
